e7f7ba8ac670384f9b8639f53a93a14c7b41a550
hatch <hatch.tech.eng@gmail.com>
2018-01-11 01:26:20 +0900

    右クリックメニュー追加

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,8 +1,19 @@
-(function() {
-	chrome.contextMenus.create({
-		title: "Open New Tab",
-		onclick: function (info, tab) {
-			chrome.tabs.create();
-		}
-	});
-}).call(this);
+
+chrome.contextMenus.create({
+	title: "新しいタブを開く ",
+	onclick: function () {
+		chrome.tabs.create({
+			url: "https://www.google.co.jp/"
+		});
+	}
+});
+
+chrome.contextMenus.create({
+	title: "aタグリンクを開く",
+    contexts: ["link"],
+	onclick: function (info) {
+		chrome.tabs.create({
+			url: info.pageUrl
+		});
+	}
+});
